f84b73b99000f4935a1edfb52104f8ec80d5653c
Nitin R Gupta <nitinramachandragupta@gmail.com>
2021-04-25 23:53:59 +0530

    Added empty state

--- src/components/WorkloadList/WorkloadList.tsx
+++ src/components/WorkloadList/WorkloadList.tsx
@@ -28,7 +28,15 @@ const WorkloadList: React.SFC<WorkloadListProps> = ({ workloads, cancelWorkload,
       <div className="workload-list">
         {!workloads.length 
         ? (
-            <div className="empty-list">No workloads to display</div> 
+            <div className="empty-list">
+              <svg width="302" height="106" viewBox="0 0 302 106" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect width="302" height="106" rx="4" fill="#111111"/>
+              <rect x="26" y="29" width="121" height="13" fill="#2C2C2C"/>
+              <rect x="26" y="64" width="121" height="13" fill="#2C2C2C"/>
+              <circle cx="252.5" cy="57.5" r="19.5" fill="#2C2C2C"/>
+              </svg>
+              <p>No ongoing workloads currently</p>
+            </div> 
           )
         : 
           workloads.map((workload) => (
